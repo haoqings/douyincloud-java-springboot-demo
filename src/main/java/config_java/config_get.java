7a14cf7f1015b73a3bfb4b2483993a8e72b41f1b
shisan123 <8005991+shisan979797@user.noreply.gitee.com>
2025-01-07 20:58:22 +0800

    1

--- src/main/java/config_java/config_get.java
+++ src/main/java/config_java/config_get.java
@@ -10,6 +10,7 @@ public class config_get {
 
         // 获取配置对象
         DycConfig config = DycConfigCenter.getDycConfig();
+
         String res = config.getStringValue("test-key", "default_value");
 
         // 获取指定key的值
@@ -17,5 +18,4 @@ public class config_get {
         System.out.println("value: " + value);
         return res;
     }
-
 }
